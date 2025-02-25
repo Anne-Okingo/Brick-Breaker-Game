7c20396ed462ac9a77cacea8a1d23fed3a3392fe
Alice Okingo <annekadiso@gmail.com>
2025-02-25 10:35:37 +0300

    feat: add logic for adding the bricks dynamically, handle collution, deletion when hit

--- src/brick.js
+++ src/brick.js
@@ -1,33 +1,31 @@
 import { detectCollision } from "./collisionDetection.js";
 
 export default class Brick {
-  constructor(game, position) {
-    this.image = document.getElementById("img_brick");
-
-    this.game = game;
-
-    this.position = position;
-    this.width = 80;
-    this.height = 24;
-
-    this.markedForDeletion = false;
-  }
-
-  update() {
-    if (detectCollision(this.game.ball, this)) {
-      this.game.ball.speed.y = -this.game.ball.speed.y;
-
-      this.markedForDeletion = true;
+    constructor(game, position) {
+        this.game = game;
+        this.position = position;
+        this.width = 80;
+        this.height = 24;
+        
+        this.markedForDeletion = false;
+        
+        // Create a DOM element for this brick
+        this.element = document.createElement('div');
+        this.element.className = 'brick';
+        this.element.style.left = `${this.position.x}px`;
+        this.element.style.top = `${this.position.y}px`;
+        
+        // Add the brick to the DOM
+        document.getElementById('bricks-container').appendChild(this.element);
     }
-  }
-
-  draw(ctx) {
-    ctx.drawImage(
-      this.image,
-      this.position.x,
-      this.position.y,
-      this.width,
-      this.height
-    );
-  }
+    
+    update() {
+        if (detectCollision(this.game.ball, this)) {
+            this.game.ball.speed.y = -this.game.ball.speed.y;
+            this.markedForDeletion = true;
+            
+            // Remove the brick from the DOM
+            this.element.remove();
+        }
+    }
 }
