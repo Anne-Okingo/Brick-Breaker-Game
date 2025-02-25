b05a6b16b99dddc890e1242b65a3b19b673d0dea
Alice Okingo <annekadiso@gmail.com>
2025-02-25 10:39:19 +0300

    feat: add feat to prevent the apddle from going out of bounds

--- src/paddle.js
+++ src/paddle.js
@@ -1,43 +1,49 @@
 export default class Paddle {
-    constructor(game) {
+  constructor(game) {
       this.gameWidth = game.gameWidth;
-  
+      this.element = document.getElementById('paddle');
+      
       this.width = 150;
       this.height = 20;
-  
+      
       this.maxSpeed = 7;
       this.speed = 0;
-  
+      
       this.position = {
-        x: game.gameWidth / 2 - this.width / 2,
-        y: game.gameHeight - this.height - 10
+          x: game.gameWidth / 2 - this.width / 2,
+          y: game.gameHeight - this.height - 10
       };
-    }
-  
-    moveLeft() {
-      this.speed = -this.maxSpeed;
-    }
-  
-    moveRight() {
-      this.speed = this.maxSpeed;
-    }
-  
-    stop() {
-      this.speed = 0;
-    }
-  
-    draw(ctx) {
-      ctx.fillStyle = "#0ff";
-      ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
-  
-    update(deltaTime) {
-      this.position.x += this.speed;
-  
-      if (this.position.x < 0) this.position.x = 0;
-  
-      if (this.position.x + this.width > this.gameWidth)
-        this.position.x = this.gameWidth - this.width;
-    }
+      
+      this.updatePosition();
   }
   
+  moveLeft() {
+      this.speed = -this.maxSpeed;
+  }
+  
+  moveRight() {
+      this.speed = this.maxSpeed;
+  }
+  
+  stop() {
+      this.speed = 0;
+  }
+  
+  update(deltaTime) {
+      this.position.x += this.speed;
+      
+      // Prevent paddle from going out of bounds
+      if (this.position.x < 0) this.position.x = 0;
+      if (this.position.x + this.width > this.gameWidth) 
+          this.position.x = this.gameWidth - this.width;
+          
+      this.updatePosition();
+  }
+  
+  updatePosition() {
+      this.element.style.left = `${this.position.x}px`;
+      this.element.style.top = `${this.position.y}px`;
+      this.element.style.width = `${this.width}px`;
+      this.element.style.height = `${this.height}px`;
+  }
+}
